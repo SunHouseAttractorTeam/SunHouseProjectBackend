cebe73568e355fefde3626be236e9ea440d9dd1c
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-11-09 21:49:56 +0600

    SUN-20 рефакторинг

--- models/Category.js
+++ models/Category.js
@@ -3,16 +3,11 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
     title:{
-        type: String,
-        required: true
-    },
-    course: {
         type: Schema.Types.ObjectId,
         required: true,
         ref: "Course"
     }
 });
-
 
 const Category = mongoose.model('Category', CategorySchema);
 
