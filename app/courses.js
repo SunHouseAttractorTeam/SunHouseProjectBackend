16a373547a7cf411b3ec9b1b7a41136f7c2b636e
Zanokha Olga <o.zanokha@gmail.com>
2022-11-10 20:56:20 +0600

    SUN-23 Написала роут на реадктирование

--- app/courses.js
+++ app/courses.js
@@ -64,5 +64,42 @@ router.post('/', auth, permit('teacher'), async (req, res) => {
     }
 });
 
+router.put('/:id', auth, permit('teacher'), async(req,  res) => {
+    const { title, description, category, price, time } = req.body;
+    const user = req.user;
+
+    if (!title || !description || !category || !price || !time) {
+        return res.status(401).send({message: 'Data not valid!'});
+    }
+
+    const courseData = {
+        title,
+        description,
+        category,
+        user: req.user._id,
+        price,
+        time,
+    };
+
+    try {
+        const course = await Course.findById(req.params.id);
+
+        if(!course) {
+           return res.status(404).send({message: 'Course not found!'});
+        }
+
+        if(course.user._id !== user._id) {
+            return res.status(401).send({message: 'Wrong token!'});
+        }
+
+        const updateCourse = await Course
+            .findOneAndUpdate(req.params.id, courseData);
+
+        res.send(updateCourse);
+    }  catch (e) {
+        res.sendStatus(500);
+    }
+});
+
 
 module.exports = router;
