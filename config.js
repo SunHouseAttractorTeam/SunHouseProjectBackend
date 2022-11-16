d6ab856e87dc187437fedf682d214e76b573e938
aman <amanmaksat0@gmail.com>
2022-11-16 22:59:28 +0600

    SUN-25 добавил вк-апи и роут для авторизации через вк.

--- config.js
+++ config.js
@@ -13,7 +13,16 @@ module.exports = {
     },
   },
   facebook: {
-    appId: '5674587392603120',
+    appId: '5912743682160832',
     appSecret: process.env.FACEBOOK_APP_SECRET,
+  },
+  google: {
+    clientId: '320044799333-r151su7af1nmosequ05tmalg8n83tvtl.apps.googleusercontent.com',
+    appSecret: process.env.GOOGLE_APP_SECRET,
+  },
+  vk: {
+    appId: '51471263',
+    personalToken: process.env.PERSONAL_TOKEN,
+    appSecret: process.env.VK_APP_SECRET,
   },
 }
