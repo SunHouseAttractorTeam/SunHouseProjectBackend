531a03dd778ea9f9d1051bea34c0172e98b97762
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-11-17 22:49:53 +0600

    SUN-60 сделана модель уведомлений

---
+++ models/Notification.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const NotificationSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+        enum: ['warning', 'success', 'info'],
+    },
+    description: {
+        required: true,
+        type: String
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+})
+
+const Notification = mongoose.model('Notification', NotificationSchema)
+
+module.exports = Notification
