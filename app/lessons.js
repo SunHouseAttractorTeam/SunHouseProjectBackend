c977dbda71c13e83ca0ec8f2ea3cfaa813c1e068
Zanokha Olga <o.zanokha@gmail.com>
2022-11-24 18:24:47 +0600

    SUN-68 Рефактор get, getById, post

--- app/lessons.js
+++ app/lessons.js
@@ -1,128 +1,158 @@
 const express = require('express')
-const auth = require("../middleweare/auth");
-const Lesson = require("../models/Lesson");
-const permit = require("../middleweare/permit");
+const auth = require('../middleweare/auth')
+const Lesson = require('../models/Lesson')
+const Module = require('../models/Module')
+const permit = require('../middleweare/permit')
 
 const router = express.Router()
 
 router.get('/', auth, async (req, res) => {
-    try {
-        const lesson = await Lesson.find()
-        return res.send(lesson)
-    } catch (e) {
-        return res.status(500).send({ error: e.message })
-    }
+  const query = {}
+
+  if (req.query.module) query.module = req.query.module
+
+  try {
+    const lesson = await Lesson.find(query)
+    return res.send(lesson)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
 router.get('/:id', auth, async (req, res) => {
-    try {
-        const findLesson = await Lesson.findById(req.params.id)
+  try {
+    const findLesson = await Lesson.findById(req.params.id)
 
-        if (!findLesson) {
-            return res.status(404).send({ message: 'Lesson not found!' })
-        }
-
-        return res.send(findLesson)
-    } catch (e) {
-        return res.status(500).send({ error: e.message })
+    if (!findLesson) {
+      return res.status(404).send({ message: 'Lesson not found!' })
     }
 
+    return res.send(findLesson)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
-router.post('/', auth, permit('admin', 'teacher'),   async (req, res) => {
-    try {
-        const {title, description,file, audio, video, module} = req.body
+router.post('/', auth, permit('admin', 'teacher'), async (req, res) => {
+  const moduleId = req.query.module
 
-        if (!title && !description && !module ) {
-            return res.status(400).send({
-                message: 'Data not valid',
-            })
-        }
+  try {
+    const { title, description } = req.body
+    const module = await Module.findById(moduleId)
 
-        if(!audio && !video && !file){
-            return res.status(400).send({
-                message: 'Provide file,audio or video',
-            })
-        }
-
-        const lessonData = {
-            title,
-            description
-        }
-        if (audio) lessonData.audio = audio
-        if (video) lessonData.video = video
-        if (file) lessonData.file = file
-
-        const lesson = new Lesson(lessonData)
-        await lesson.save()
-
-        // TODO: need to wait SUN-70 task for use model Module
-        // Need to save lesson model to the Module.data
-
-        return res.send(lesson)
-    } catch (e) {
-        return res.status(500).send({ error: e.message })
+    if (!module) {
+      return res.status(404).send('There are no such module!')
     }
+
+    if (!title && !description) {
+      return res.status(400).send({
+        message: 'Data not valid',
+      })
+    }
+
+    const lessonData = {
+      title,
+      description,
+    }
+
+    if (req.file) {
+      switch (req.file.type) {
+        case 'file':
+          lessonData.file = req.file.file
+          break
+        case 'video':
+          lessonData.video = req.file.file
+          break
+        case 'audio':
+          lessonData.audio = req.file.file
+          break
+        default:
+          return lessonData
+      }
+    }
+
+    const lesson = new Lesson(lessonData)
+    await lesson.save()
+
+    module.data.push({
+      id: lessonData._id,
+      type: lessonData.type,
+      title: lessonData.title,
+    })
+
+    await module.save()
+    return res.send(lesson)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
-router.put('/:id',auth, permit('admin', 'teacher'),  async (req, res) => {
-    try {
-        const {title, description,file, audio, video, module } = req.body
+router.put('/:id', auth, permit('admin', 'teacher'), async (req, res) => {
+  const moduleId = req.query.module
+  try {
+    const { title, description } = req.body
 
-        if (!title && !description && !module) {
-            return res.status(400).send({
-                message: 'Data not valid',
-            })
-        }
-
-        if(!audio && !video && !file){
-            return res.status(400).send({
-                message: 'Provide file,audio or video',
-            })
-        }
-
-        const lessonData = {
-            title,
-            description,
-            file,
-            audio,
-            video
-        }
-
-        const lesson = await Lesson.findById(req.params.id)
-
-        if (!lesson) {
-            return res.status(404).send({ message: 'Lesson not found!' })
-        }
-
-        const updateLesson = await Lesson.findByIdAndUpdate(req.params.id, lessonData, { new: true })
-
-        // TODO: need to wait SUN-70 task for use model Module
-        // Need to update lesson model in the Module.data array
-
-        return res.send(updateLesson)
-    } catch (e) {
-        return  res.status(500).send({ error: e.message })
+    if (!title && !description) {
+      return res.status(400).send({
+        message: 'Data not valid',
+      })
     }
+
+    const lessonData = {
+      title,
+      description,
+    }
+
+    if (req.file) {
+      switch (req.file.type) {
+        case 'file':
+          lessonData.file = req.file.file
+          break
+        case 'video':
+          lessonData.video = req.file.file
+          break
+        case 'audio':
+          lessonData.audio = req.file.file
+          break
+        default:
+          return lessonData
+      }
+    }
+
+    const lesson = await Lesson.findById(req.params.id)
+
+    if (!lesson) {
+      return res.status(404).send({ message: 'Lesson not found!' })
+    }
+
+    const updateLesson = await Lesson.findByIdAndUpdate(req.params.id, lessonData, { new: true })
+
+    // TODO: need to wait SUN-70 task for use model Module
+    // Need to update lesson model in the Module.data array
+
+    return res.send(updateLesson)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
-router.delete('/:id' ,auth,  permit('admin', 'teacher'), async (req, res) => {
-    try {
-        const lesson = await Lesson.findById(req.params.id)
+router.delete('/:id', auth, permit('admin', 'teacher'), async (req, res) => {
+  try {
+    const lesson = await Lesson.findById(req.params.id)
 
-        if (!lesson) {
-            return res.status(404).send({ message: 'Lesson not found!' })
-        }
-
-        const deleteLesson = await Lesson.findByIdAndDelete({ _id: req.params.id })
-
-        // TODO: need to wait SUN-70 task for use model Module
-        // Need to delete lesson model from Module.data array
-
-        return res.send(deleteLesson)
-    } catch (e) {
-        return res.status(500).send({ error: e.message })
+    if (!lesson) {
+      return res.status(404).send({ message: 'Lesson not found!' })
     }
+
+    const deleteLesson = await Lesson.findByIdAndDelete({ _id: req.params.id })
+
+    // TODO: need to wait SUN-70 task for use model Module
+    // Need to delete lesson model from Module.data array
+
+    return res.send(deleteLesson)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
 module.exports = router
