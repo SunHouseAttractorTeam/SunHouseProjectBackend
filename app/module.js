7029d55eb9c0a776af63e5600560aa7052350e06
Kylymbek Shaihidinov <shaihidinovkylymbek@gmail.com>
2022-11-26 15:11:34 +0600

    SUN-70 Доделал модель и роуты для Модулей

--- app/module.js
+++ app/module.js
@@ -1,96 +1,97 @@
 const express = require('express')
+
 const router = express.Router()
-const dayjs = require("dayjs");
+const dayjs = require('dayjs')
 
 const Module = require('../models/Module')
+const Course = require('../models/Course')
 
+const auth = require('../middleweare/auth')
+const permit = require('../middleweare/permit')
 
-const auth = require("../middleweare/auth")
-const permit = require("../middleweare/permit")
+router.get('/', auth, async (req, res) => {
+  try {
+    const modules = await Module.find()
 
-router.get('/', auth, permit('admin', 'teacher'), async (req, res) => {
-    try {
-
-        const modules = await Module.find()
-
-        res.send(modules)
-    } catch (e) {
-        return res.status(500).send({error: e.message})
-    }
+    return res.send(modules)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
-router.get('/:id', auth, permit('admin', 'teacher'), async (req, res) => {
-    try {
+router.get('/:id', auth, async (req, res) => {
+  const { id } = req.params
+  try {
+    const modules = await Module.findById(id)
 
-        const {id} = req.params
-
-        const modules = await Module.findById(id)
-
-        res.send(modules)
-    } catch (e) {
-        return res.status(500).send({error: e.message})
-    }
+    return res.send(modules)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
 router.post('/', auth, permit('admin', 'teacher'), async (req, res) => {
-    try {
-        const {title, course} = req.body
-        if (!title || !course) {
-            return res.status(400).send({error: 'Data not valid'})
-        }
+  try {
+    const { title } = req.body
+    const { course } = req.query
 
-        const modulesData = {
-            title,
-            course,
-            datetime: dayjs().format('DD/MM/YYYY'),
-        }
-
-        const modules = new Module(modulesData)
-        await modules.save()
-
-        res.send(modules)
-    } catch (e) {
-        return res.status(500).send({error: e.message})
+    if (!title || !course) {
+      return res.status(400).send({ error: 'Data not valid' })
     }
+
+    const modulesData = {
+      title,
+      course,
+      datetime: dayjs().format('DD/MM/YYYY'),
+    }
+
+    const modules = new Module(modulesData)
+    await modules.save()
+    await Course.findByIdAndUpdate(course, { $push: { modules: modules._id } })
+
+    return res.send(modules)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
 router.put('/:id', auth, permit('admin', 'teacher'), async (req, res) => {
-    try {
-        const {id} = req.params
-        const {title} = req.body
+  try {
+    const { id } = req.params
+    const { title } = req.body
 
-        const modules = await Module.findById(id)
+    const modules = await Module.findById(id)
 
-        if (!modules) {
-            return res.status(400).send({message: 'Module not found'})
-        }
-
-        modules.title = title
-
-        modules.save()
-        res.send(modules)
-    } catch (e) {
-        return res.status(500).send({error: e.message})
+    if (!modules) {
+      return res.status(400).send({ message: 'Module not found' })
     }
+
+    modules.title = title
+
+    modules.save()
+    return res.send(modules)
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
 router.delete('/:id', async (req, res) => {
-    try {
-        const {id} = req.params
-        const modules = await Module.findById(id)
+  try {
+    const { id } = req.params
+    const { course } = req.query
+    const modules = await Module.findById(id)
 
-        if (!modules) {
-            return res.status(401).send({message: 'Not found'})
-        }
-
-        await Module.deleteOne(modules)
-
-        res.send({message: 'Success'})
-    } catch (e) {
-        return res.status(500).send({error: e.message})
+    if (!modules) {
+      return res.status(401).send({ message: 'Not found' })
     }
 
+    await Course.findByIdAndUpdate(course, { $pull: { modules: id } })
+    await Module.deleteOne(modules)
+
+    return res.send({ message: 'Success' })
+  } catch (e) {
+    return res.status(500).send({ error: e.message })
+  }
 })
 
-
-module.exports = router;
+module.exports = router
--- models/Module.js
+++ models/Module.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+
+const { Schema } = mongoose
 
 const ModuleSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    course: {
-        type: Schema.Types.ObjectId,
-        ref: 'Course',
-        required: true
-    },
-    datetime:{
-        type: String,
-        required: true,
-    },
-    data:[]
+  title: {
+    type: String,
+    required: true,
+  },
+  course: {
+    type: Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true,
+  },
+  datetime: {
+    type: String,
+    required: true,
+  },
+  data: [],
 })
 
-const Module = mongoose.model('Module',ModuleSchema)
+const Module = mongoose.model('Module', ModuleSchema)
 
 module.exports = Module
