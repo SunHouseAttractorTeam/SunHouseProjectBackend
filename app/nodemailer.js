7537dd51180677590187cc8bb079cdadf6d8af7b
Ilim Alybekov <ilimalybekov2204@gmail.com>
2022-12-01 20:09:52 +0600

    SUN-77 Функция отправки подтвержения почты сделано

--- app/nodemailer.js
+++ app/nodemailer.js
@@ -1,26 +1,31 @@
+require("dotenv").config()
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport(
-  {
-    host: 'smtp.mail.ru',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.USER_GMAIL,
-      pass: process.env.APP_PASSWORD,
+    {
+        host: 'smtp.mail.ru',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.USER_GMAIL,
+            pass: process.env.APP_PASSWORD,
+        },
     },
-  },
-  {
-    from: 'Mailer Test <ilimalybekov@mail.ru>',
-  },
-)
+);
 
-const mailer = message => {
-  transporter.sendMail(message, (err, info) => {
-    if (err) return console.log(err)
-    console.log('Email sent: ', info)
-    return info
-  })
-}
-
-module.exports = mailer
+module.exports.sendConfirmationCode = (name, email, confirmationCode) => {
+    console.log('NAME: ', name)
+    console.log('email: ', email)
+    console.log('confirmationCode: ', confirmationCode)
+    transporter.sendMail({
+        from: process.env.USER_GMAIL,
+        to: email,
+        subject: "Please confirm your account",
+        html: `
+        <h1>Email confirmation</h1>
+        <h2>Hello ${name}</h2>
+        <p>Thank for your attention. Please confirm your email by clicking on the following link</p>
+        <a href="http://localhost:3000/confirm/${confirmationCode}">Click here</a>
+      `
+    }).catch(err => console.error(err));
+};
