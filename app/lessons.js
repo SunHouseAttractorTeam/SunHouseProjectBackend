c32d567e857cb5e56ccf740afbe2fde581ff653d
smbv1 <105828216+smbv1@users.noreply.github.com>
2022-12-01 19:47:38 +0600

    Update lessons.js

--- app/lessons.js
+++ app/lessons.js
@@ -76,9 +76,9 @@ router.post('/', auth, permit('admin', 'teacher'), async (req, res) => {
     await lesson.save()
 
     module.data.push({
-      id: lessonData._id,
-      type: lessonData.type,
-      title: lessonData.title,
+      id: lesson._id,
+      type: lesson.type,
+      title: lesson.title,
     })
 
     await module.save()
