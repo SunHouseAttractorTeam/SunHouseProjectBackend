b3c13fda104daec2a4500acb947f6dec43abfd8a
Ilim Alybekov <ilimalybekov2204@gmail.com>
2022-12-03 00:26:55 +0600

    SUN-77 Пофиксил eslint

--- app/nodemailer.js
+++ app/nodemailer.js
@@ -1,31 +1,31 @@
-require("dotenv").config()
+require('dotenv').config()
 const nodemailer = require('nodemailer')
 
-const transporter = nodemailer.createTransport(
-    {
-        host: 'smtp.mail.ru',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.USER_GMAIL,
-            pass: process.env.APP_PASSWORD,
-        },
-    },
-);
+const transporter = nodemailer.createTransport({
+  host: 'smtp.mail.ru',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.USER_GMAIL,
+    pass: process.env.APP_PASSWORD,
+  },
+})
 
 module.exports.sendConfirmationCode = (name, email, confirmationCode) => {
-    console.log('NAME: ', name)
-    console.log('email: ', email)
-    console.log('confirmationCode: ', confirmationCode)
-    transporter.sendMail({
-        from: process.env.USER_GMAIL,
-        to: email,
-        subject: "Please confirm your account",
-        html: `
+  console.log('NAME: ', name)
+  console.log('email: ', email)
+  console.log('confirmationCode: ', confirmationCode)
+  transporter
+    .sendMail({
+      from: process.env.USER_GMAIL,
+      to: email,
+      subject: 'Please confirm your account',
+      html: `
         <h1>Email confirmation</h1>
         <h2>Hello ${name}</h2>
         <p>Thank for your attention. Please confirm your email by clicking on the following link</p>
         <a href="http://localhost:3000/confirm/${confirmationCode}">Click here</a>
-      `
-    }).catch(err => console.error(err));
-};
+      `,
+    })
+    .catch(err => console.error(err))
+}
--- app/users.js
+++ app/users.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const axios = require('axios')
-const {nanoid} = require('nanoid')
-const {VKAPI} = require('vkontakte-api')
-const {OAuth2Client} = require('google-auth-library')
+const { nanoid } = require('nanoid')
+const { VKAPI } = require('vkontakte-api')
+const { OAuth2Client } = require('google-auth-library')
 const User = require('../models/User')
 const config = require('../config')
 const nodemailer = require('./nodemailer')
@@ -12,257 +12,248 @@ const router = express.Router()
 const utils = require('../middleweare/token')
 
 const getLiveCookie = user => {
-    const {username} = user
-    const maxAge = 730 * 60 * 60
-    return {token: utils.getToken(username, maxAge), maxAge}
+  const { username } = user
+  const maxAge = 730 * 60 * 60
+  return { token: utils.getToken(username, maxAge), maxAge }
 }
 
 const getLiveSecretCookie = user => {
-    const {username} = user
-    const maxAge = 5 * 60 * 60
-    return {token: utils.getToken(username, maxAge), maxAge}
-};
+  const { username } = user
+  const maxAge = 5 * 60 * 60
+  return { token: utils.getToken(username, maxAge), maxAge }
+}
 
 router.post('/', async (req, res) => {
-    try {
-        const secretToken = getLiveSecretCookie({email: req.body.email})
-        const {email, password, username} = req.body
+  try {
+    const secretToken = getLiveSecretCookie({ email: req.body.email })
+    const { email, password, username } = req.body
 
-        if (!email || !password || !username) {
-            return res.status(400).send({error: 'Data not valid'})
-        }
-
-        const userData = {email, password, username, confirmationCode: secretToken.token}
-
-        const user = new User(userData)
-
-        const {token, maxAge} = getLiveCookie(user)
-
-        // res.cookie('jwt', token, {
-        //     httpOnly: false,
-        //     maxAge: maxAge * 1000,
-        // })
-
-        user.token = token
-
-        console.dir(user, {depth: null, maxArrayLength: null})
-
-        await user.save();
-
-        nodemailer.sendConfirmationCode(
-            user.username,
-            user.email,
-            user.confirmationCode
-        );
-
-        console.log('USER: ', user)
-
-
-        return res.status(201).send(user)
-    } catch (e) {
-        console.log(e)
-        return res.status(400).send(e)
+    if (!email || !password || !username) {
+      return res.status(400).send({ error: 'Data not valid' })
     }
+
+    const userData = { email, password, username, confirmationCode: secretToken.token }
+
+    const user = new User(userData)
+
+    const { token, maxAge } = getLiveCookie(user)
+
+    // res.cookie('jwt', token, {
+    //     httpOnly: false,
+    //     maxAge: maxAge * 1000,
+    // })
+
+    user.token = token
+
+    console.dir(user, { depth: null, maxArrayLength: null })
+
+    await user.save()
+
+    nodemailer.sendConfirmationCode(user.username, user.email, user.confirmationCode)
+
+    return res.status(201).send(user)
+  } catch (e) {
+    console.log(e)
+    return res.status(400).send(e)
+  }
 })
 
-router.get("/confirm/:confirmationCode", (req, res) => {
-    User.findOne({
-        confirmationCode: req.params.confirmationCode
+router.get('/confirm/:confirmationCode', (req, res) => {
+  User.findOne({
+    confirmationCode: req.params.confirmationCode,
+  })
+    .then(user => {
+      if (!user) {
+        return res.status(404).send({ message: 'User Not Found' })
+      }
+
+      user.authentication = true
+      user.save(err => {
+        if (err) {
+          res.status(500).send({ message: err })
+        }
+      })
     })
-        .then((user) => {
-            if (!user) {
-                return res.status(404).send({message: "User Not Found"})
-            }
-
-            user.status = "Active"
-            user.save((err) => {
-                if (err) {
-                    res.status(500).send({message: err})
-
-                }
-            })
-        })
-        .catch((err) => console.log("error", err))
+    .catch(err => console.log('error', err))
 })
-
 
 router.post('/sessions', async (req, res) => {
-    try {
-        if (req.cookies.jwt) {
-            const user = await User.findOne({token: req.cookies.jwt})
-            return res.send(user)
-        }
-
-        const user = await User.findOne({email: req.body.email})
-
-        if (!user) {
-            return res.status(401).send({error: 'Credentials are wrong!'})
-        }
-
-        if (user.status !== "Active") {
-            return res.status(401).send({message: 'Pending account. Please verify your email'})
-        }
-
-        const isMatch = await user.checkPassword(req.body.password)
-        if (!isMatch) {
-            res.status(401).send({error: 'Credentials are wrong!'})
-        }
-
-        const {token, maxAge} = getLiveCookie(user)
-
-        res.cookie('jwt', token, {
-            httpOnly: false,
-            maxAge: maxAge * 1000,
-        })
-
-        user.token = token
-        await user.save({validateBeforeSave: false})
-
-        return res.send(user)
-    } catch (e) {
-        return res.status(500)
+  try {
+    if (req.cookies.jwt) {
+      const user = await User.findOne({ token: req.cookies.jwt })
+      return res.send(user)
     }
+
+    const user = await User.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(401).send({ error: 'Credentials are wrong!' })
+    }
+
+    if (user.authentication !== true) {
+      return res.status(401).send({ message: 'Pending account. Please verify your email' })
+    }
+
+    const isMatch = await user.checkPassword(req.body.password)
+    if (!isMatch) {
+      res.status(401).send({ error: 'Credentials are wrong!' })
+    }
+
+    const { token, maxAge } = getLiveCookie(user)
+
+    res.cookie('jwt', token, {
+      httpOnly: false,
+      maxAge: maxAge * 1000,
+    })
+
+    user.token = token
+    await user.save({ validateBeforeSave: false })
+
+    return res.send(user)
+  } catch (e) {
+    return res.status(500)
+  }
 })
 
 router.post('/facebookLogin', async (req, res) => {
-    const inputToken = req.body.accessToken
-    const accessToken = `${config.facebook.appId}|${config.facebook.appSecret}`
+  const inputToken = req.body.accessToken
+  const accessToken = `${config.facebook.appId}|${config.facebook.appSecret}`
 
-    const debugTokenUrl = `https://graph.facebook.com/debug_token?input_token=${inputToken}&access_token=${accessToken}`
+  const debugTokenUrl = `https://graph.facebook.com/debug_token?input_token=${inputToken}&access_token=${accessToken}`
 
-    try {
-        const response = await axios.get(debugTokenUrl)
+  try {
+    const response = await axios.get(debugTokenUrl)
 
-        if (response.data.data.error) {
-            return res.status(401).send({message: 'Facebook token incorrect!'})
-        }
-
-        if (req.body.userID !== response.data.data.user_id) {
-            return res.status(401).send({message: 'Wrong User ID'})
-        }
-        let user = await User.findOne({$or: [{facebookId: req.body.userID}, {email: req.body.email}]})
-
-        if (!user) {
-            user = new User({
-                email: req.body.email,
-                password: nanoid(),
-                facebookId: req.body.userID,
-                username: req.body.name,
-                avatar: req.body.picture.data.url,
-                authentication: true,
-            })
-        }
-        const {token, maxAge} = getLiveCookie(user)
-        res.cookie('jwt', token, {
-            httpOnly: false,
-            maxAge: maxAge * 1000,
-        })
-
-        user.token = token
-
-        await user.save({validateBeforeSave: false})
-        return res.send(user)
-    } catch (e) {
-        console.log(e)
-        return res.status(401).send({message: 'Facebook token incorrect!'})
+    if (response.data.data.error) {
+      return res.status(401).send({ message: 'Facebook token incorrect!' })
     }
+
+    if (req.body.userID !== response.data.data.user_id) {
+      return res.status(401).send({ message: 'Wrong User ID' })
+    }
+    let user = await User.findOne({ $or: [{ facebookId: req.body.userID }, { email: req.body.email }] })
+
+    if (!user) {
+      user = new User({
+        email: req.body.email,
+        password: nanoid(),
+        facebookId: req.body.userID,
+        username: req.body.name,
+        avatar: req.body.picture.data.url,
+        authentication: true,
+      })
+    }
+    const { token, maxAge } = getLiveCookie(user)
+    res.cookie('jwt', token, {
+      httpOnly: false,
+      maxAge: maxAge * 1000,
+    })
+
+    user.token = token
+
+    await user.save({ validateBeforeSave: false })
+    return res.send(user)
+  } catch (e) {
+    console.log(e)
+    return res.status(401).send({ message: 'Facebook token incorrect!' })
+  }
 })
 
 router.post('/vkLogin', async (req, res) => {
-    const api = new VKAPI({
-        accessToken: config.vk.personalToken,
+  const api = new VKAPI({
+    accessToken: config.vk.personalToken,
+  })
+
+  try {
+    const { user } = req.body.session
+    const ticket = await api.users.get({ user_ids: [user.id], fields: ['photo_max_orig'] })
+
+    if (ticket.length === 0 || ticket[0].id !== parseInt(user.id, 10)) {
+      return res.status(401).send({ message: 'VK token incorrect!' })
+    }
+
+    let userIs = await User.findOne({ $or: [{ vkId: user.id }, { email: req.body.email }] })
+
+    if (!userIs) {
+      userIs = new User({
+        email: `${user.id}@gmail.com`,
+        password: nanoid(),
+        vkId: user.id,
+        username: `${user.first_name} ${user.last_name}`,
+        avatar: ticket[0].photo_max_orig,
+        authentication: true,
+      })
+    }
+
+    const { token, maxAge } = getLiveCookie(user)
+
+    res.cookie('jwt', token, {
+      httpOnly: false,
+      maxAge: maxAge * 1000,
     })
 
-    try {
-        const {user} = req.body.session
-        const ticket = await api.users.get({user_ids: [user.id], fields: ['photo_max_orig']})
+    await userIs.save({ validateBeforeSave: false })
 
-        if (ticket.length === 0 || ticket[0].id !== parseInt(user.id, 10)) {
-            return res.status(401).send({message: 'VK token incorrect!'})
-        }
-
-        let userIs = await User.findOne({$or: [{vkId: user.id}, {email: req.body.email}]})
-
-        if (!userIs) {
-            userIs = new User({
-                email: `${user.id}@gmail.com`,
-                password: nanoid(),
-                vkId: user.id,
-                username: `${user.first_name} ${user.last_name}`,
-                avatar: ticket[0].photo_max_orig,
-                authentication: true,
-            })
-        }
-
-        const {token, maxAge} = getLiveCookie(user)
-
-        res.cookie('jwt', token, {
-            httpOnly: false,
-            maxAge: maxAge * 1000,
-        })
-
-        await userIs.save({validateBeforeSave: false})
-
-        return res.send(userIs)
-    } catch (e) {
-        return res.status(401).send({message: 'VK token incorrect!'})
-    }
+    return res.send(userIs)
+  } catch (e) {
+    return res.status(401).send({ message: 'VK token incorrect!' })
+  }
 })
 
 router.post('/googleLogin', async (req, res) => {
-    const {credential, clientId} = req.body
-    try {
-        const ticket = await client.verifyIdToken({
-            idToken: `${credential}`,
-            audience: clientId,
-        })
+  const { credential, clientId } = req.body
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: `${credential}`,
+      audience: clientId,
+    })
 
-        const {name, email, picture, sub} = ticket.payload
+    const { name, email, picture, sub } = ticket.payload
 
-        let user = await User.findOne({email})
+    let user = await User.findOne({ email })
 
-        if (!user) {
-            user = new User({
-                email,
-                password: nanoid(),
-                username: name,
-                avatar: picture,
-                googleId: sub,
-                authentication: true,
-            })
-        }
-
-        const {token, maxAge} = getLiveCookie(user)
-
-        res.cookie('jwt', token, {
-            httpOnly: false,
-            maxAge: maxAge * 1000,
-        })
-
-        user.token = token
-        await user.save({validateBeforeSave: false})
-        return res.send(user)
-    } catch (e) {
-        return res.status(401).send({message: 'Google token incorrect!'})
+    if (!user) {
+      user = new User({
+        email,
+        password: nanoid(),
+        username: name,
+        avatar: picture,
+        googleId: sub,
+        authentication: true,
+      })
     }
+
+    const { token, maxAge } = getLiveCookie(user)
+
+    res.cookie('jwt', token, {
+      httpOnly: false,
+      maxAge: maxAge * 1000,
+    })
+
+    user.token = token
+    await user.save({ validateBeforeSave: false })
+    return res.send(user)
+  } catch (e) {
+    return res.status(401).send({ message: 'Google token incorrect!' })
+  }
 })
 
 router.delete('/sessions', async (req, res) => {
-    const success = {message: 'Success'}
-    const cookie = req.cookies.jwt
+  const success = { message: 'Success' }
+  const cookie = req.cookies.jwt
 
-    if (!cookie) return res.send(success)
+  if (!cookie) return res.send(success)
 
-    const user = await User.findOne({token: cookie})
+  const user = await User.findOne({ token: cookie })
 
-    if (!user) return res.send(success)
+  if (!user) return res.send(success)
 
-    const {token} = getLiveCookie(user)
-    user.token = token
+  const { token } = getLiveCookie(user)
+  user.token = token
 
-    await user.save({validateBeforeSave: false})
+  await user.save({ validateBeforeSave: false })
 
-    return res.send({success, user})
+  return res.send({ success, user })
 })
 
 module.exports = router
